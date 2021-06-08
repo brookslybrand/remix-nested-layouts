7d87256e7c54941adb06bf27f6ec267be6354f41
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-06-08 12:51:01 -0500

    Update routes/index.tsx

--- app/routes/index/index.tsx
+++ app/routes/index/index.tsx
@@ -3,6 +3,7 @@ export default function Index() {
     <>
       <h1 className="text-5xl">This is the home page</h1>
       <p className="mt-4 text-lg">Feel free to go somewhere else</p>
+      <p>Just testing if the preview deployments work</p>
     </>
   );
 }
